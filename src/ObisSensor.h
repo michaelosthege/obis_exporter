3aed8580c411a6e661a9758a50f5a23ce33deb6c
Michael Osthege <michael.osthege@outlook.com>
2022-12-28 00:26:55 +0100

    Extract function to get gauges from raw message

--- src/ObisSensor.h
+++ src/ObisSensor.h
@@ -10,6 +10,66 @@ const byte START_SEQUENCE[] = {0x2F};
 const byte END_SEQUENCE[] = {0x21, 0x0D, 0x0A};
 const size_t BUFFER_SIZE = 3840;
 
+
+void extract_gauges(std::list<Gauge *> *gauges, size_t last_message_size, char *buffer)
+{
+    String obis_code = "";
+    String metric_name = "";
+    String metric_help = "";
+    String value = "";
+
+    // Extract current metrics from message
+    uint8 phase = 0;  // 0: searching, 1: obis code, 2: value
+    for (size_t i = 0; i < last_message_size; i++)
+    {
+        // Transitions between segments of the input text
+        if (phase == 0 && buffer[i] == ':')
+        {
+            // OBIS code coming up
+            phase = 1;
+            obis_code = "";
+            value = "";
+        }
+        else if (phase == 1 && buffer[i] == '*')
+        {
+            // End of OBIS code
+            phase = 2;
+        }
+        else if (phase == 2 && buffer[i] == '(')
+        {
+            // Value coming up
+            phase = 3;
+        }
+        else if (phase == 3 && (buffer[i] == '*' || buffer[i] == ')'))
+        {
+            // Summarize this as a Prometheus Gauge object
+            metric_name = "obis_" + obis_code;
+            metric_name.replace('.', '_');
+            metric_help = get_obis_help(obis_code);
+            Gauge *g = new Gauge(metric_name, metric_help);
+            g->set(value);
+
+            // Collect
+            gauges->push_back(g);
+
+            // back to "search" phase
+            phase = 0;
+        }
+        else
+        {
+            // Not a transition character -> Collect
+            if (phase == 1)
+            {
+                obis_code += char(buffer[i]);
+            }
+            else if (phase == 3)
+            {
+                value += char(buffer[i]);
+            }
+        }
+    }
+}
+
 class ObisSensor
 {
     public:
@@ -62,69 +122,10 @@ class ObisSensor
         }
 
         gauges->clear();
-        extract_gauges(gauges, last_message_size);
-        
+        extract_gauges(gauges, last_message_size, (char*) buffer);
 
         metrics = render(gauges);
 
         Serial.println(metrics);
-
-    void extract_gauges(std::list<Gauge *> *gauges, size_t last_message_size)
-    {
-        String obis_code = "";
-        String metric_name = "";
-        String metric_help = "";
-        String value = "";
-
-        // Extract current metrics from message
-        uint8 phase = 0;  // 0: searching, 1: obis code, 2: value
-        for (size_t i = 0; i < last_message_size; i++)
-        {
-            // Transitions between segments of the input text
-            if (phase == 0 && buffer[i] == ':')
-            {
-                // OBIS code coming up
-                phase = 1;
-                obis_code = "";
-                value = "";
-            }
-            else if (phase == 1 && buffer[i] == '*')
-            {
-                // End of OBIS code
-                phase = 2;
-            }
-            else if (phase == 2 && buffer[i] == '(')
-            {
-                // Value coming up
-                phase = 3;
-            }
-            else if (phase == 3 && (buffer[i] == '*' || buffer[i] == ')'))
-            {
-                // Summarize this as a Prometheus Gauge object
-                metric_name = "obis_" + obis_code;
-                metric_name.replace('.', '_');
-                metric_help = get_obis_help(obis_code);
-                Gauge *g = new Gauge(metric_name, metric_help);
-                g->set(value);
-
-                // Collect
-                gauges->push_back(g);
-
-                // back to "search" phase
-                phase = 0;
-            }
-            else
-            {
-                // Not a transition character -> Collect
-                if (phase == 1)
-                {
-                    obis_code += char(buffer[i]);
-                }
-                else if (phase == 3)
-                {
-                    value += char(buffer[i]);
-                }
-            }
-        }
     }
 };
